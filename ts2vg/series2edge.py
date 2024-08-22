4a0d36bdb8491e0f77cff1324350cdc6f36943c7
Shengzhong Mao <71408242+szmao1@users.noreply.github.com>
2024-08-22 06:06:21 +0100

    Add files

--- ts2vg/series2edge.py
+++ ts2vg/series2edge.py
@@ -5,7 +5,7 @@ from visibility_graph import visibility_graph
 import sys
 from networkx import write_edgelist
 
-# read time series from file given or from STDIN
+#read time series from file given or from STDIN
 series = []
 for line in fileinput.input():
     series.append( float(line.strip()) )
